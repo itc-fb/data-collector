06008739b8cea3fc458aa8a2bc846aeb1cfb1408
Gor1194 <gorchshmarityan94@gmail.com>
2019-09-25 23:15:55 +0400

    -New initial structure

--- FB_Data_collector_Project/src/main/java/fb/DataCollectorProject/MainPage.java
+++ FB_Data_collector_Project/src/main/java/fb/DataCollectorProject/MainPage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class MainPage extends LoginPage {
+class MainPage extends LoginPage {
 
-    public MainPage(WebDriver driver) {
+    MainPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
@@ -15,8 +15,7 @@ public class MainPage extends LoginPage {
     @FindBy(css = "#u_0_a > div:nth-child(1) > div:nth-child(1) > div > a")
     private WebElement userProfileButton;
 
-    public MainPage userProfileButtonClick(){
+    void userProfileButtonClick(){
         userProfileButton.click();
-        return this;
     }
 }
