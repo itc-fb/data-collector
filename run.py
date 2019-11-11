8dad48add55c771efca59c85459846fe29b3e855
Gor1194 <gorchshmarityan94@gmail.com>
2019-11-11 16:57:27 +0400

    Added file path to run.py arguments

--- run.py
+++ run.py
@@ -2,20 +2,26 @@ import os
 import csv
 import sys
 
-chrome = "chrome"
-
 def setData():
-    with open('userLoginAndPass.txt') as csv_file:
+    if(len(sys.argv) == 2):
+        filePath = sys.argv[1]
+        isFile = os.path.isfile(filePath)
+        if(isFile):
+            openFile(sys.argv[1])
+        else:
+            defaultDataFile = "userLoginAndPass.txt"
+            openFile(defaultDataFile)
+
+def openFile(path):
+    with open(path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             login = row[0]
             password = row[1]
-            if(len(sys.argv) > 1 and sys.argv[1] == chrome):
-                runMaven(login, password, chrome)
-            else:
-                runMaven(login, password)
+            runMaven(login, password)
 
-def runMaven(login, password, browser = chrome):
+def runMaven(login, password):
+    browser = "chrome"
     mvnCommand = "mvn package -Dlogin={} -Dpassword={} -Dbrowser={}".format(login, password, browser)
     os.system(mvnCommand)
 
