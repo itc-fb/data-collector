27758d890c47cd0d2b41ece618306875505efc0e
Gor1194 <gorchshmarityan94@gmail.com>
2019-11-11 21:34:50 +0400

    Changed argv to argparse

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 import os
 import csv
 import sys
+import argparse
 
 def setData():
-    if(len(sys.argv) == 2):
-        filePath = sys.argv[1]
-        isFile = os.path.isfile(filePath)
+    parser = argparse.ArgumentParser(epilog="Example of usage: 'python run.py --usersFilePath ../Desktop/example.txt'")
+    parser.add_argument("--usersFilePath", help="Get file path.")
+    args = parser.parse_args()
+    if(args.usersFilePath is not None):
+        path = args.usersFilePath
+        isFile = os.path.isfile(path)
         if(isFile):
-            openFile(sys.argv[1])
+            openFile(path)
         else:
-            print("Please type correct file path.")
+            print("Please see the usage by '--help' key.")
     else:
         openFile("userLoginAndPass.txt")
 
